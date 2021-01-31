3e3d75a81ff16d5926d21fa2dad06d0e29585728
Gor Stepanyan <gor.stepanyan@sflpro.com>
2021-01-31 16:05:22 +0400

    Create

---
+++ TilesetsCore/TilesetsCore.h
@@ -0,0 +1,18 @@
+//
+//  TilesetsCore.h
+//  TilesetsCore
+//
+//  Created by Gor Stepanyan on 31.01.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TilesetsCore.
+FOUNDATION_EXPORT double TilesetsCoreVersionNumber;
+
+//! Project version string for TilesetsCore.
+FOUNDATION_EXPORT const unsigned char TilesetsCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TilesetsCore/PublicHeader.h>
+
+
